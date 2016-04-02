f4b8e25b42bdf7f40add82930d4854240d1bc03a
pburrows <pburrows@burrowscorp.com>
2016-04-02 15:20:34 -0400

    start of data model

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This project is a static web site generator written in C#, using ASP.Net MVC, WebApi and Angular. 
 
-The intent behind this project is to create an easy to use static site generator suitable for the running of an online magazine. As such, features like managing articles through a lifecycle, and generating multiple "issues" will be incorporated. 
+The intent behind this project is to create an easy to use static site generator suitable for the running of an online magazine. As such, features like managing articles through a lifecycle, and generating multiple "issues" will be incorporated.
+
+This project is in its infancy. I do not recommend attempting to use it at this point. 
---
+++ src/BTC.SiteGenerator.Website/Data/Domain/DataModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTC.SiteGenerator.Website.Data.Domain
+{
+    public class Publication
+    {
+        public int PublicationId { get; set; }
+    }
+
+    public class Issue
+    {
+        public int IssueId { get; set; }
+    }
+
+    public class Article
+    {
+        public int ArticleId { get; set; }
+        public virtual ArticleTemplate ArticleTemplate { get; set; }
+        public int ArticleTemplateId { get; set; }
+    }
+
+    public class ArticleTemplate
+    {
+        public ArticleTemplate()
+        {
+            Sections = new List<ArticleTemplateSection>();
+        }
+
+        public int ArticleTemplateId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<ArticleTemplateSection> Sections { get; set; }
+    }
+
+    public class ArticleTemplateSection
+    {
+        public int ArticleTemplateSectionId { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+    }
+}
